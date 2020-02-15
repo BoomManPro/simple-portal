370912c44ca656c5f3146708a8b7a91cdeb90f94
wangqimeng <wangqimeng@mininglamp.com>
2020-02-15 11:13:40 +0800

    update name

--- src/test/java/cn/boommanpro/simple/portal/SimplePortalApplicationTests.java
+++ src/test/java/cn/boommanpro/simple/portal/SimplePortalApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JenkinsPortalApplicationTests {
+class SimplePortalApplicationTests {
 
     @Test
     void contextLoads() {
